926e290e8350f351da9c943ee56fa2248cc0a52a
Charles <ellerkaresz@gmail.com>
2019-11-21 17:52:00 +0100

    add drop boundaries to icons

--- src/components/DesktopIcon.js
+++ src/components/DesktopIcon.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const DesktopIcon = ({ icon, title, onDoubleClick }) => {
   const dropIcon = e => {
+    const top = e.clientY - (e.target.clientHeight / 2);
+    const left = e.clientX - (e.target.clientWidth / 2);
+    const rightBoundary = window.innerWidth - e.target.clientWidth - 16;
+    const bottomBoundary = window.innerHeight - e.target.clientHeight - 64;
+
     e.target.style.position = 'absolute';
-    e.target.style.top = `${e.clientY - (e.target.clientHeight / 2)}px`;
-    e.target.style.left = `${e.clientX - (e.target.clientWidth / 2)}px`;
+    e.target.style.top = `${top > window.innerHeight - 120 ? bottomBoundary : (top < 0 ? 0 : top)}px`;
+    e.target.style.left = `${left > window.innerWidth - 120 ? rightBoundary : (left < 0 ? 0 : left)}px`;
   }
 
   return (
