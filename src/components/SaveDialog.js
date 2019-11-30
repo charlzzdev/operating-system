8731088e9f5dea97832df077ab27d7fd695eeb90
Charles <ellerkaresz@gmail.com>
2019-11-30 14:18:47 +0100

    replace file if it already exists

--- src/components/SaveDialog.js
+++ src/components/SaveDialog.js
@@ -9,17 +9,24 @@ const SaveDialog = ({ text, setShowSaveDialog }) => {
     fileName.current.select();
   }, []);
 
+  const save = () => {
+    const fileNameExists = fileStore.getState().some(file => file.name === fileName.current.value);
+    const file = { name: fileName.current.value, content: text };
+
+    if (!fileNameExists) {
+      fileStore.dispatch({ type: 'save', file });
+    } else {
+      fileStore.dispatch({ type: 'replace', file });
+    }
+
+    setShowSaveDialog(false);
+  }
+
   return (
     <div className="SaveDialog">
       <button className="CloseDialog" onClick={() => setShowSaveDialog(false)}><Close /></button>
       <input type="text" defaultValue="untitled" ref={fileName} />
-      <button onClick={() => {
-        fileStore.dispatch({
-          type: 'save',
-          file: { name: fileName.current.value, content: text }
-        });
-        setShowSaveDialog(false);
-      }}>Save</button>
+      <button onClick={save}>Save</button>
     </div>
   )
 }
--- src/stores/fileStore.js
+++ src/stores/fileStore.js
@@ -3,6 +3,12 @@ import { createStore } from 'redux';
 function fileSaver(state = [], action) {
   switch (action.type) {
     case 'save': return [...state, action.file];
+    case 'replace': return state.map(file => {
+      if (file.name === action.file.name) {
+        file.content = action.file.content;
+      }
+      return file;
+    });
     default: return state;
   }
 }
