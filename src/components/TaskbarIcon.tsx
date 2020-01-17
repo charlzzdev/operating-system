357278fc22c347ab7f885816a7632a02c99a599c
Charles <ellerkaresz@gmail.com>
2020-01-17 18:22:54 +0100

    change TaskbarIcon to tsx

---
+++ src/components/TaskbarIcon.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+interface TaskbarIconProps {
+  id: string,
+  width: string,
+  onClick: () => void,
+  children: object
+}
+
+const TaskbarIcon = ({ id, width, onClick, children }: TaskbarIconProps) => {
+  return (
+    <button
+      className={`TaskbarIcon icon-${id}`}
+      style={{ width }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default TaskbarIcon;
